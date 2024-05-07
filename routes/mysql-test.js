2aa0c24cf7d8ddaa3f0a6411a63fffe74d1d158c
Jürgen M <j.mosgan@gmail.com>
2024-05-07 11:15:53 +0200

    added more comments

--- routes/mysql-test.js
+++ routes/mysql-test.js
@@ -4,11 +4,13 @@ const db = require("../config/mysql_db").promise();
 async function runQuery() {
   try {
     const queries = [
-      "SELECT * FROM city WHERE IsD = 12",
+      "SELECT * FROM city WHERE ID = 12",
       "SELECT * FROM country WHERE Name = 'Austria'",
     ];
+    //execute all the queries concurrently using Promise.all
+    //Promise.all waits for all promises to be fulfilled and returns an array of their results
     const results = await Promise.all(queries.map((query) => db.query(query)));
-    return results.map(([rows, fields]) => rows);
+    return results.map(([rows, fields]) => rows); //map over the results and extract the rows from each query result
   } catch (err) {
     console.error(err);
     const error = {
