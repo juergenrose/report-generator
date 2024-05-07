89f1992c1732d478f65ab98ef111ac45ac8fb7aa
Jürgen M <j.mosgan@gmail.com>
2024-05-07 10:14:35 +0200

    added more detailed error handling

--- routes/mysql-test.js
+++ routes/mysql-test.js
@@ -4,14 +4,24 @@ const db = require("../config/mysql_db").promise();
 async function runQuery() {
   try {
     const queries = [
-      "SELECT * FROM city WHERE IsD = 12", 
+      "SELECT * FROM city WHERE IsD = 12",
       "SELECT * FROM country WHERE Name = 'Austria'",
     ];
     const results = await Promise.all(queries.map((query) => db.query(query)));
     return results.map(([rows, fields]) => rows);
   } catch (err) {
     console.error(err);
-    return { data: null, error: err.message };
+    const error = {
+      data: null,
+      error: {
+        message: err.message,
+        sqlState: err.sqlState,
+        errno: err.errno,
+        code: err.code,
+        sqlMessage: err.sqlMessage,
+      },
+    };
+    return error;
   }
 }
 
