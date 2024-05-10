9e81939d2734e067abebb4854eda61dec3bbd88a
Jürgen M <j.mosgan@gmail.com>
2024-05-10 12:57:52 +0200

    added pdf download function with pdfkit

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const port = 3000;
 const fs = require("fs");
 const fsPromise = require("fs").promises;
 const path = require("path");
-const { exec } = require("child_process");
-const jsonCsv = require("json2csv");
-const { create } = require("xmlbuilder2");
 
+const jsonCsv = require("json2csv");
+const PDFDocument = require("pdfkit");
 
 app.get("/report", (req, res) => {
   res.send("now you can search for a report");
@@ -72,67 +71,55 @@ app.get("/report/:reportname/csv", async (req, res, next) => {
 });
 
 
-
-/* **********************************
-* THE NEXT PART IS JUST FOR TESTING!*
-*********************************** */
-
-//convert json in xml, download report as xml and covert it to pdf
-app.get("/report/:reportname/xml", async (req, res, next) => {
+//download report as pdf
+app.get("/report/:reportname/pdf", async (req, res, next) => {
   try {
     const reportname = req.params.reportname;
     const { runReport } = require(`./routes/${reportname}`);
     if (typeof runReport === "function") {
-      const jsonData = await runReport(reportname);
-      const root = create({ version: '1.0' }).ele(reportname); //create a single root element
-      //add child elements for each record
-      jsonData.forEach((record, index) => {
-        const recordElement = root.ele(`record_${index}`); //use prefix for element names
-        //assuming each record is represented as key-value pairs in the json
+      const dbData = await runReport(); //call runReport function to fetch data
+
+      //create a new pdf document
+      const doc = new PDFDocument();
+
+      res.setHeader("Content-Type", "application/pdf");
+      res.setHeader(
+        "Content-Disposition",
+        `attachment; filename=${reportname}.pdf`
+      );
+      // Write PDF content to the response
+      doc.pipe(res);
+
+      doc.text(`Report ${reportname}`, {
+      });
+      //write each record to the pdf
+      dbData.forEach((record, index) => {
+        doc.moveDown();
+        doc.text(`Record ${index + 1}:`);
+        doc.moveDown(); //move to next line
         Object.entries(record).forEach(([key, value]) => {
-          const xmlKey = key.replace(/^\d/, '_$&'); //add underscore prefix if key starts with a digit
-          if (typeof value === "object" && value !== null) {
-            //if value is an object, recursively create child elements
-            Object.entries(value).forEach(([subKey, subValue]) => {
-              const subXmlKey = subKey.replace(/^\d/, '_$&');
-              recordElement.ele(subXmlKey).txt(subValue);
-            });
-          } else {
-            //if value is not an object, create a text node
-            recordElement.ele(xmlKey).txt(value);
-          }
+          doc.text(`${key}: ${value}`);
         });
+        doc.moveDown();
       });
-      const xmlData = root.end({ prettyPrint: true });
-      const filename = `${reportname}.xml`;
-      const xmlPath = path.join(__dirname, "xml", filename);
-      fs.writeFileSync(xmlPath, xmlData); //write xml data to file
 
-      const fopPath = path.join(__dirname, 'fop/fop', 'fop.cmd');
-      const pdfPath = path.join(__dirname, "pdf", reportname + ".pdf");
-      const xslPath = path.join(__dirname, "styles", "report-style.xsl");
-      const cmd = `${fopPath} -xml ${xmlPath} -xsl ${xslPath} -pdf ${pdfPath}`;
-
-      //execute fop cmd
-      exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-          console.error(`Error executing Apache FOP: ${error}`);
-          res.status(500).send(`Error executing Apache FOP: ${error}`);
-          return;
-        }
-        console.log(`PDF generated successfully for ${reportname}`);
-        const pdfPath = path.join(__dirname, "pdf", reportname + ".pdf");
-        res.download(pdfPath); //send pdf file as response
-      });
+      //finalize the pdf
+      doc.end();
     } else {
       throw new Error(`runReport not found for ${reportname}`);
     }
   } catch (err) {
-    console.error(`Error fetching report data for ${req.params.reportname}`, err);
-    res.status(500).send(`Error fetching report data for ${req.params.reportname}, error: ${err.message}`);
+    console.error(
+      `error fetching report data for ${req.params.reportname}`,
+      err
+    );
+    res
+      .status(500)
+      .send(
+        `error fetching report data for ${req.params.reportname}, error: ${err.message}`
+      );
   }
 });
-
 
 //port listening
 app.listen(port, () => {
