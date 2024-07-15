6674172537ebd06f605e8bcd909cd981f2039226
Jürgen M <j.mosgan@gmail.com>
2024-07-15 12:38:06 +0200

    added second mssql db config

--- config/mssql_db.js
+++ config/mssql_db.js
@@ -1,11 +1,9 @@
 const sql = require("mssql");
-
-//dotenv config
 const dotenv = require("dotenv");
 dotenv.config();
 
-//mssql config
-const config = {
+// MSSQL config for first database
+const config1 = {
   server: process.env.MSSQL_HOST,
   authentication: {
     type: "default",
@@ -21,14 +19,44 @@ const config = {
   },
 };
 
-//connect to mssql db
-sql
-  .connect(config)
-  .then(() => {
-    console.log("connected to mssql db");
-  })
-  .catch((err) => {
-    console.log("connection to mssql db failed!", err);
-  });
+// MSSQL config for second database
+const config2 = {
+  server: process.env.MSSQL_HOST,
+  authentication: {
+    type: "default",
+    options: {
+      userName: process.env.MSSQL_USER,
+      password: process.env.MSSQL_PASSWORD,
+    },
+  },
+  options: {
+    encrypt: false,
+    database: process.env.MSSQL_DATABASE2,
+    trustServerCertificate: true,
+  },
+};
 
-  module.exports = config;
+// Create connection pools
+const pool1 = new sql.ConnectionPool(config1);
+const pool2 = new sql.ConnectionPool(config2);
+
+async function connectToDatabases() {
+  try {
+    await pool1.connect();
+    console.log("connected to mssql1 db");
+    await pool2.connect();
+    console.log("connected to mssql2 db");
+  } catch (err) {
+    console.log("connection to one of the dbs failed!", err);
+  }
+}
+
+async function ensureConnected(pool) {
+  if (!pool.connected) {
+    await pool.connect();
+  }
+}
+
+connectToDatabases();
+
+module.exports = { pool1, pool2, sql, ensureConnected };
